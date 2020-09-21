9b07ddbb19c2a5a03a97ebe649459b4dcc7ac42a
hchaudhari73 <hchaudhari73@gmail.com>
2020-09-21 21:10:37 +0530

    to run scripts

--- run.sh
+++ run.sh
@@ -6,4 +6,8 @@ elif [ $1 = "lm" ]; then
     python log_model.py
 elif [ $1 = "pred" ]; then
     python prediction.py
+elif [ $1 = "dl" ]; then
+    python keras_model.py
+elif [ $1 = "ep" ]; then
+    python embedding_pred.py
 fi
