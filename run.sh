3c63179395dfd866526dacb5e761dd4d1b0aad73
hchaudhari73 <hchaudhari73@gmail.com>
2020-09-21 11:12:30 +0530

    prediction file added

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
-python model.py
+echo $1
+
+if [ $1 = "model" ]; then 
+    python model.py; 
+elif [ $1 = "lm" ]; then
+    python log_model.py
+elif [ $1 = "pred" ]; then
+    python prediction.py
+fi
